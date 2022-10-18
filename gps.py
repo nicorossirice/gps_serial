1e36ee28e074b43e6f49a3ead3eb3cc17b3948cb
Nico Rossi <ncr3@rice.edu>
2022-10-18 03:59:27 +0000

    Added more comments, safeties, and options

--- gps.py
+++ gps.py
@@ -2,13 +2,22 @@ from serial import Serial
 
 from dataclasses import dataclass
 from datetime import datetime, timezone
+from enum import Enum, auto
 from math import cos, sin, atan2, pi
+
+class NMEASentenceType(Enum):
+	GPRMC = auto()
+	GGA = auto()
+
 
 @dataclass
 class GPRMC:
+	"""
+	Represents a single GPRMC sentence.
+	"""
 	time: float
 	valid: bool
-	latitidue: float
+	latitude: float
 	longitude: float
 	speed: float
 	course: float
@@ -18,29 +27,79 @@ class GPRMC:
 	magnetic_variation_dir: str
 	checksum: str
 
+
+@dataclass
+class GGA:
+	"""
+	Placeholder
+	"""
+	valid: bool
+
+
+@dataclass
+class NMEA:
+	sentence_type: NMEASentenceType
+	sentence: str
+	parsed: "GPRMC | GGA"
+
+
 class GPS:
-	def __init__(self, port: str):
-		self.ser = Serial(port)
+	"""
+	Class used to read incoming serial data from the GPS through the BeagleBone Black.
+	"""
 
-	def setup(self):
-		self.ser.write(b"$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n")
-		print(self.readline())
-		self.ser.write(b"PMTK220,1000*1F\r\n") # 1Hz
-		# self.ser.write(b"$PMTK220,200*2C\r\n") # 5Hz
-		print(self.readline())
-	def readline(self):
-		return self.ser.readline()
+	def __init__(self, port: str, baud_rate: int = 9600, timeout: float = 1.5):
+		"""
+		Initialize the serial port, getting it ready for reading. The timeout value is very important, since readline can block until it hits the timeout value. If no timeout value is desired, use None.
+		"""
+		self.ser = Serial(port, baud_rate, timeout=timeout)
 
-	def checksum(self, data: str):
+
+	def gps_setup(self, enable_gga: bool = False, rate_5Hz: bool = False):
+		"""
+		Sets up the GPS for general use. Defaults to to only enabling GPRMC sentences which are emitted at 1Hz.
+		"""
+		if enable_gga:
+			self.ser.write(b"$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n")
+		else:
+			self.ser.write(b"$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n")
+
+		if rate_5Hz:
+			print("Enabling 5Hz")
+			self.ser.write(b"$PMTK220,200*2C\r\n") # 5Hz
+		else:
+			print("Enabling 1Hz")
+			self.ser.write(b"$PMTK220,1000*1F\r\n") # 1Hz
+
+
+	def readline(self) -> str:
+		"""
+		Returns a single valid line from the serial port and returns it as a string. In this case, valid means that it is decodable into a string. This function will repeatedly read new lines from the input until a valid string is encountered. The one exception is if getting a new line times out, in which case None is returned.
+		"""
+		while True:
+			line = self.ser.readline()
+			if line == b'':
+				return None
+			try:
+				return line.decode()
+			except UnicodeDecodeError:
+				pass
+		 
+
+	def checksum(self, data: str) -> str:
 		'''
-		Calculates the checksum of a string. If creating a command, the string is everything between the '$' and the '*'.
+		Calculates the checksum of a string in hexadecimal, returned as a string. If creating a command, the string is everything between the '$' and the '*'.
 		'''
 		checksum = 0
 		for c in data:
-			checksum ^= int(c)
-		return checksum
+			checksum ^= ord(c)
+		return str(hex(checksum))
 
-	def get_bearing(self, cur_lat, cur_long, target_lat, target_long):
+
+	def get_bearing(self, cur_lat: float, cur_long: float, target_lat: float, target_long: float) -> float:
+		"""
+		Gets the bearing in degrees between two lat-long points. This is the straight line direction from the cur position to the target position, relative to a line directly towards the north pole.
+		"""
 		x = cos(target_lat * pi / 180) * sin((target_long - cur_long) * pi / 180)
 		print(x)
 		y = cos(cur_lat * pi / 180) * sin(target_lat * pi / 180) - sin(cur_lat * pi / 180) * cos(target_lat * pi / 180) * cos((target_long - cur_long) * pi / 180)
@@ -48,40 +107,78 @@ class GPS:
 		print(y)
 		return atan2(x, y)
 
-	def parse_gprmc(self, nmea_sentence: "str | bytes"):
+
+	def parse_nmea_sentence(self, nmea_sentence: "str | bytes") -> NMEA:
+		"""
+		Parses a NMEA sentence into a dataclass for easier manipulation. The currently supported NMEA sentences are as follows:
+			- GPRMC
+		A NMEA dataclass is returned on success, or None on failure. 
+		"""
 		if type(nmea_sentence) is bytes:
 			nmea_sentence = nmea_sentence.decode()
 		if nmea_sentence[:6] == "$GPRMC":
 			comps = nmea_sentence[7:-3].split(",")
 			print(comps)
+			time_float = float(comps[0])
+			valid = comps[1] == "A"
+
+			if valid:
+				latitude = float(comps[2][:2]) + float(comps[2][2:]) / 60.0
+				longitude = float(comps[4][:3]) + float(comps[4][3:]) / 60.0
+				latitude *= 1 if comps[3] == "N" else -1
+				longitude *= 1 if comps[5] == "E" else -1
+				magnetic_variation = float(comps[9])
+				magnetic_variation_dir = comps[10]
+			else:
+				latitude = None
+				longitude = None
+				magnetic_variation = float("nan")
+				magnetic_variation_dir = None
+
+			speed = float(comps[6])
+			course = float(comps[7])
+			date = int(comps[8])
+			datetime_obj = datetime(int(comps[8][4:]), int(comps[8][2:4]), int(comps[8][0:2]), int(comps[0][0:2]), int(comps[0][2:4]), int(comps[0][4:6]), int(comps[0][7:]), tzinfo=timezone.utc)
+			checksum = nmea_sentence[-2:]
+
 			gprmc = GPRMC(
-				float(comps[0]),
-				comps[1] == "A",
-				float(comps[2][:2]) + float(comps[2][2:]) / 60.0,
-				float(comps[4][:3]) + float(comps[4][3:]) / 60.0,
-				float(comps[6]),
-				float(comps[7]),
-				int(comps[8]),
-				datetime(int(comps[8][4:]), int(comps[8][2:4]), int(comps[8][0:2]), int(comps[0][0:2]), int(comps[0][2:4]), int(comps[0][4:6]), int(comps[0][7:]), tzinfo=timezone.utc),
-				float(comps[9]) if not comps[9] == "" else float("nan"),
-				comps[10],
-				nmea_sentence[-2:]
+				time_float,
+				valid,
+				latitude,
+				longitude,
+				speed,
+				course,
+				date,
+				datetime_obj,
+				magnetic_variation,
+				magnetic_variation_dir,
+				checksum,
 			)
-			gprmc.latitidue *= 1 if comps[3] == "N" else -1
-			gprmc.longitude *= 1 if comps[5] == "E" else -1
+
 			return gprmc
+
+		return None
 
 
 if __name__ == "__main__":
 	gps = GPS("/dev/ttyS1")
+	gps.gps_setup(enable_gga=False, rate_5Hz=False)
+	# print(gps.checksum("PMTK220,1000"))
+	# print(str(gps.readline().decode()))
+	# print(gps.checksum("PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"))
+	# print(gps.parse_nmea_sentence("$GPRMC,194509.000,A,4042.6142,N,07400.4168,W,2.03,221.11,160412,,,A*77"))
 
-	print(gps.parse_gprmc("$GPRMC,194509.000,A,4042.6142,N,07400.4168,W,2.03,221.11,160412,,,A*77"))
+	# gps.gps_setup()
 
-	# gps.setup()
-
-	# while True:
-	# 	print(str(gps.readline().decode()))
-	# 	print("----")
+	# import time
+	# start = time.time()
+	while True:
+		nmea = gps.readline()
+		# print(time.time() - start)
+		print(nmea)
+		# print(gps.parse_nmea_sentence(nmea))
+		print("----")
+		# start = time.time()
 
 	# for _ in range(100):
 	# 	print(gps.readline())
